c56c5f578f0df2ade634fba88ce4ef1c4fa5b1b1
Siim Kallari <skallari@itcollege.ee>
2017-01-29 12:43:08 +0200

    Entities, Domain, Wrieframes of what should be done

---
+++ Wireframes/dbo.project_tasks.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[project_tasks]
+(
+	[project_task_id] INT NOT NULL PRIMARY KEY, 
+    [project_id] INT NOT NULL, 
+    [description] VARCHAR(MAX) NOT NULL, 
+    [worker_name] VARCHAR(MAX) NOT NULL, 
+    [start_date_time] DATETIME NOT NULL, 
+    [end_date_time] DATETIME NOT NULL, 
+    [time_spent] TIME NOT NULL, 
+    CONSTRAINT [FK_project_tasks_projects] FOREIGN KEY (project_id) REFERENCES [projects]([project_id])
+)
---
+++ Wireframes/dbo.projects.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[projects]
+(
+	[project_id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(MAX) NOT NULL, 
+    [description] VARCHAR(MAX) NOT NULL, 
+    [manager_name] VARCHAR(MAX) NOT NULL, 
+    [created_at] DATETIME NOT NULL
+)
